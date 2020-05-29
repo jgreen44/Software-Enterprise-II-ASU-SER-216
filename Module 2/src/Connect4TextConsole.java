098434058c24c898129bef55edad4e14b2b98bf8
jgreen44 <jgreen44@asu.edu>
2020-05-29 08:31:11 -0500

    added error checking

--- Module 2/src/Connect4TextConsole.java
+++ Module 2/src/Connect4TextConsole.java
@@ -1,11 +1,29 @@
+/**
+ * Connect4TextConsole class contains all of the logical
+ * UI elements presented to the two players of the game
+ * 
+ * @author Jason Green, jgreen44@asu.edu
+ * @version 1.0
+ */
 public class Connect4TextConsole extends Connect4 {
 
 
-    // default constructor
+    /**
+     * Instantiates a new Connect4TextConsole.
+     *
+     * @param X the x
+     * @param Y the y
+     */
+// default constructor
     public Connect4TextConsole(char X, char Y){
 
     }
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         char player = 'O';
 
@@ -14,34 +32,27 @@ public class Connect4TextConsole extends Connect4 {
 
         connect.initializeArray(grid);
         connect.drawGrid(grid);
-
+        System.out.println("Begin Game");
 
         do{
             player = connect.chooseNextPlayer(player);
-            System.out.println("Begin Game");
             System.out.println("Player " + player + " - your turn. Choose a column number from 1 - 7");
             int number = connect.scanner.nextInt();
 
 
             // check if number is between 1 and 7
-            connect.checkNumberValue(number);
-            connect.dropChip(grid, number, player);
-            connect.drawGrid(grid);
+            number = connect.checkNumberValue(number);
+
+            if (connect.validateMove(grid, number)){
+                connect.dropChip(grid, number, player);
+                connect.drawGrid(grid);
+            }
+
 
 
         }while(!connect.checkWinner(grid, player));
 
         System.out.println("Player " + player + " has won!");
-
-
-
-
-
-
-
-
-
-
 
 
     }
